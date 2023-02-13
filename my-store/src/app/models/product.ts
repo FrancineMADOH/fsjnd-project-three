0d76c9bb385d467f44699666dfc3483a014a3ae9
FrancineMADOH <francinemadoh@gmail.com>
2023-02-13 14:45:43 +0100

    .

---
+++ my-store/src/app/models/product.ts
@@ -0,0 +1,13 @@
+export interface Product {
+    
+    id?:number;
+    name: string;
+    price:number;
+    url:string;
+    description:string;
+    qty_available:number;
+    reservation?:number;
+
+
+}
+
--- my-store/src/app/product-item/product-item.component.ts
+++ my-store/src/app/product-item/product-item.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit, Input } from '@angular/core';
+import { Product } from '../models/product';
 @Component({
   selector: 'app-product-item',
   templateUrl: './product-item.component.html',
   styleUrls: ['./product-item.component.css']
 })
-export class ProductItemComponent {
+export class ProductItemComponent implements OnInit {
+
+
+  constructor(){}
+
+  ngOnInit():void{}
 
 }
